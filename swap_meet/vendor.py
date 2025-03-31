679faecf4adf0b4801b9c5a25c15011201ece251
sashavershkova <a.m.vershkova@gmail.com>
2025-03-31 12:17:16 -0700

    completed wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else: 
+            return False
