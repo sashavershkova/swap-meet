8c968f474bd27df5db9c32ba26ec5b363fe8ce6f
sashavershkova <a.m.vershkova@gmail.com>
2025-04-01 16:08:01 -0700

    add clothing class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,9 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, id=None, fabric="Unknown", condition=0):
+        super().__init__(id, condition)
+        self.fabric = fabric
+    
+    def __str__(self):
+        return super().__str__() + f" It is made from {self.fabric} fabric."
