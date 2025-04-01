4e9123b07c34836832070f8b68593a0af752807b
sashavershkova <a.m.vershkova@gmail.com>
2025-04-01 16:08:57 -0700

    add electronics class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,9 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, id=None, type="Unknown", condition=0):
+        super().__init__(id, condition)
+        self.type = type
+    
+    def __str__(self):
+        return super().__str__() + f" This is a {self.type} device."
