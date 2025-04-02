e7843fd4e304348d6fad751b7bf5e9cfe730ccf6
sashavershkova <a.m.vershkova@gmail.com>
2025-04-01 19:46:03 -0700

    add condition_description func in Item class, passed last test in wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -14,4 +14,19 @@ class Item:
     
     def __str__(self):
         return f"An object of type {self.get_category()} with id {self.id}."
+    
+    def condition_description(self):
+        if self.condition == 0:
+            return "Unusable"
+        elif self.condition == 1:
+            return "Poor"
+        elif self.condition == 2:
+            return "Fair"
+        elif self.condition == 3:
+            return "Good"
+        elif self.condition == 4:
+            return "Very good"
+        elif self.condition == 5:
+            return "Like new"
+        
 
