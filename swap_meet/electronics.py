199dcea5793b74f413f30244f817b2d15229b44a
sashavershkova <a.m.vershkova@gmail.com>
2025-04-02 22:50:33 -0700

    add age

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,8 +1,8 @@
 from swap_meet.item import Item
 
 class Electronics(Item):
-    def __init__(self, id=None, type="Unknown", condition=0):
-        super().__init__(id, condition)
+    def __init__(self, id=None, type="Unknown", condition=0, age=None):
+        super().__init__(id, condition, age)
         self.type = type
     
     def __str__(self):
